0891d380384456fe89ee8486ef2c857fb8961212
SergioSanchezN <lsergiolsanchezl@gmail.com>
2022-02-26 08:57:15 -0500

    pantalla objeto terminada

--- ucommerce_client/lib/screens/objeto.dart
+++ ucommerce_client/lib/screens/objeto.dart
@@ -9,6 +9,26 @@ class Objeto extends StatefulWidget {
 }
 
 class _Objeto extends State<Objeto> {
+  int _selectedIndex = 2;
+  int counter = 0;
+
+  void _onItemTapped(int index) {
+    print(index);
+    if(index==1){
+
+    }
+    else if(index==2){
+
+    }
+    else if(index==3){
+      
+    }
+
+
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,36 +36,166 @@ class _Objeto extends State<Objeto> {
     return Scaffold( 
       
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-      child: Container(
+      body: Column(
+        children: [
         
-        child: Column(          
+        Stack(
           children: [
             Container(
-              decoration: BoxDecoration(
-                color: Color(0xfff5f8fd),
-                borderRadius: BorderRadius.circular(30)
-              ),
-              padding: EdgeInsets.symmetric(horizontal: 24),
-              margin: EdgeInsets.symmetric(horizontal: 24),
-              child: Row(
-                children: <Widget>[
-                Expanded( 
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: "buscar",
-                      border: InputBorder.none
-                    ),
-                  ),
-                ),
-                  Icon(Icons.search),
-                ],),
+            height: 350,
+            child: ClipRRect(            
+            borderRadius: BorderRadius.circular(16),
+            child: Image.network("https://i.pinimg.com/originals/93/b5/f9/93b5f9913d2e4316cd6e541c67b9aed0.jpg", fit: BoxFit.cover,),
             ),
-            SizedBox(height: 16),           
-            SizedBox(height: 16),
-            TargetList()
+          ),
+          Column(
+            children: [
+              SizedBox(height: 16),
+              
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  MaterialButton(                   
+                    color: Colors.grey[400],
+                    shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0)),
+                    onPressed: () {}, 
+                    child: Icon(Icons.arrow_back_ios),       
+                  ),
+                  SizedBox(width: 180,),
+                  FlatButton(
+                    color: Colors.grey[400],
+                    shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0)),
+                    onPressed: () {}, 
+                    child: Row(
+                      children: [
+                        Icon(Icons.shopping_cart),
+                        Icon(Icons.list),
+                      ],
+                    ),       
+                  ),
+                ],
+              ),
+          ],),                    
+        ],), 
+        SizedBox(height: 8), 
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Galletas con chispas", style: TextStyle(fontSize: 30),),
+            MaterialButton(                   
+                    color: Colors.white,
+                    shape: CircleBorder(side: BorderSide(width: 0)),
+                    padding: EdgeInsets.all(0),
+                    onPressed: () {}, 
+                    child: Icon(Icons.favorite, color: Colors.pink[200]),      
+                  ),
         ],),
-      ),
+        SizedBox(height: 8),
+        Row(
+          children: [
+            SizedBox(width: 8),
+            Text("Luisa alvares", style: TextStyle(fontSize: 15),),
+          ],
+        ),
+        Row(         
+          children: [
+            SizedBox(width: 8),
+            Text("Ricas galletas de sabor a vainilla con chispas de \n chocolate", style: TextStyle(fontSize: 15),),
+          ],
+        ),
+        Row(
+          children: [
+            SizedBox(width: 8),
+            Text("Todos los dias a las 4p.m. en biblioteca, no te lo \n pierdas", style: TextStyle(fontSize: 15),),
+          ],
+        ),
+        SizedBox(height: 16),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Text("Unidades", style: TextStyle(fontSize: 15),),
+            MaterialButton(                   
+                    color: Colors.white,
+                    shape: CircleBorder(side: BorderSide(width: 0)),
+                    padding: EdgeInsets.all(0),
+                    onPressed: () {}, 
+                    child: Icon(Icons.add),      
+                  ),
+            Text("1", style: TextStyle(fontSize: 15),),
+            MaterialButton(                   
+                    color: Colors.white,
+                    shape: CircleBorder(side: BorderSide(width: 0)),
+                    padding: EdgeInsets.all(0),
+                    onPressed: () {}, 
+                    child: Icon(Icons.remove),      
+                  ),
+          ],
+        ),
+        SizedBox(height: 16),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("\$3.6k", style: TextStyle(fontSize: 15, color: Colors.blue),),
+            SizedBox(width: 16),
+            MaterialButton(                   
+                    color: Colors.blue[900],
+                    shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0)),
+                    padding: EdgeInsets.all(0),
+                    onPressed: () {}, 
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Icon(Icons.shopping_cart),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text("añadir a la cesta"),
+                        )
+                      ],
+                    ),      
+                  ),
+            
+          ],
+        ),
+
+
+      ],),
+
+      bottomNavigationBar: BottomNavigationBar(
+        
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            
+            icon: Icon(Icons.home),
+            label: 'Home',  
+            backgroundColor: Colors.amber,         
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.store),
+            label: 'Ventas',
+            backgroundColor: Colors.amber,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shop),
+            label: 'Compra',
+            backgroundColor: Colors.amber,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.supervised_user_circle_sharp),
+            label: 'Perfil',
+            backgroundColor: Colors.amber,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.purple[900],
+        unselectedItemColor: Colors.white,
+        backgroundColor: Colors.amber,
+        onTap: _onItemTapped,
       ),
     );
   }
