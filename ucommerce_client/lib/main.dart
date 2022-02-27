fa5f6ccfde5dd9a892e9925bca463ff72e19798a
Camilo <juancmaringcu@gmail.com>
2022-02-26 23:02:08 -0500

    Se reestructuro la capa de presentacion

--- ucommerce_client/lib/main.dart
+++ ucommerce_client/lib/main.dart
@@ -1,26 +1,16 @@
-
 import 'package:flutter/material.dart';
-import 'package:ucommerce_client/screens/home_screen.dart';
-import 'package:ucommerce_client/screens/explorar.dart';
-import 'package:ucommerce_client/screens/objeto.dart';
-import 'package:ucommerce_client/screens/carro.dart';
-import 'package:ucommerce_client/screens/perfil.dart';
-import 'package:ucommerce_client/screens/admin.dart';
+
+import 'presentation/screens/screens.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner:  false,
-      home: Admin()
-
-    );
+    return const MaterialApp(debugShowCheckedModeBanner: false, home: Admin());
   }
 }
---
+++ ucommerce_client/lib/presentation/screens/screens.dart
@@ -0,0 +1,6 @@
+export 'package:ucommerce_client/presentation/screens/home_screen.dart';
+export 'package:ucommerce_client/presentation/screens/explorar.dart';
+export 'package:ucommerce_client/presentation/screens/objeto.dart';
+export 'package:ucommerce_client/presentation/screens/carro.dart';
+export 'package:ucommerce_client/presentation/screens/perfil.dart';
+export 'package:ucommerce_client/presentation/screens/admin.dart';
